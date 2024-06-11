d929e3ba89b7a6401a4fb4d554b06f653309154e
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-06-11 08:37:20 -0300

    Creating a project, Modules and Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  
+  constructor() {
+    
+    this.nomePortal='http://loiane.training'
+   
+  }
+
+  ngOnInit(): void {
+  }
+
+}
